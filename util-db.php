d59696745f400e33a4f88373d1e87af6f4ac112b
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-02 14:54:26 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,17 +1,12 @@
 <?php
-function get_db_connection() {
-    $servername = "finalproject4013.mysql.database.azure.com"; //azure hostname
-    $username = "jacobrod03";
-    $password = "Sakiboy$1";
-    $dbname = "project4013"; //name of db inside MYSQL
-    $port = 3306;
-
-    $conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('138.197.17.168', 'jacobrod_jacobrod03', 'Database3909', 'jacobrod_project4013');
     
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
     return $conn;
 }
 ?>
