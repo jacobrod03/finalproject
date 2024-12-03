173c8117406961fe1ca55e3f2485be113eac5298
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 13:21:49 -0600

    Update model-employees-with-teams.php

--- model-employees-with-teams.php
+++ model-employees-with-teams.php
@@ -12,4 +12,19 @@ function selectEmployees() {
         throw $e;
     }
 }
+
+function selectTeamsbyEmployee($eid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.team_id, team_number, team_description, location, email FROM `team` t join company c on c.team_id = t.team_id where c.employee_id =?");
+        $stmt->bind_param("i", $eid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
