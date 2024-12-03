3356acb1f38ce685ba825615acddb306aa587e9f
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 14:25:29 -0600

    Create view-teams-newform.php

---
+++ view-teams-newform.php
@@ -0,0 +1,31 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newTeamModal">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
+    </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newTeamModal" tabindex="-1" aria-labelledby="newTeamModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newTeamModalLabel">New team</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="tNumber" class="form-label">Team number</label>
+            <input type="text" class="form-control" id="tNumber" name="tNumber">
+          </div>
+          <div class="mb-3">
+            <label for="tDesc" class="form-label">Team description</label>
+            <input type="text" class="form-control" id="tDesc" name="tDesc">
+          </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
