a3fef455d95e9fd49baad2c31e12b6e928259507
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 16:31:08 -0600

    Update employees.php

--- employees.php
+++ employees.php
@@ -4,6 +4,15 @@ require_once("model-employees.php");
 
 $pageTitle = "Employees";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertEmployee($_POST['eName'], $_POST['ePhone']);
+      break;
+  }
+}
+
 $employees = selectEmployees();
 include "view-employees.php";
 include "view-footer.php";
