f9b31a015f4c6b0bdcd1d3633cc2c53284e057ef
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 12:54:32 -0600

    Create view-company-by-teams.php

---
+++ view-company-by-teams.php
@@ -0,0 +1,29 @@
+<h1>Company by teams</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Number</th>
+      <th>Description</th>
+      <th>Location</th>
+      <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($company = $companys ->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $company['team_id']; ?></td>
+    <td><?php echo $company['team_number']; ?></td>
+    <td><?php echo $company['team_description']; ?></td>
+    <td><?php echo $company['location']; ?></td>
+    <td><?php echo $company['email']; ?></td>
+  </tr>
+  <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
