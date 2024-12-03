a3919a5d519e697e1ed7725698d08280ecb9c76f
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 13:04:39 -0600

    Update view-company-by-teams.php

--- view-company-by-teams.php
+++ view-company-by-teams.php
@@ -12,7 +12,7 @@
     </thead>
     <tbody>
 <?php
-while ($company = $companies ->fetch_assoc()) {
+while ($company = $company ->fetch_assoc()) {
   ?>
   <tr>
     <td><?php echo $company['team_id']; ?></td>
