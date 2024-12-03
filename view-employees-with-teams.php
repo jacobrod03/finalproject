b2eaaa3211667c4bd70a82f6e6e8dd7339f26b45
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 13:38:39 -0600

    Update view-employees-with-teams.php

--- view-employees-with-teams.php
+++ view-employees-with-teams.php
@@ -7,14 +7,17 @@ while ($employee = $employees->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
       <p class="card-text">
+     <ul class="list-group">
 <?php
   $teams = selectTeamsByEmployee($employee['employee_id']);
   while ($team = $teams->fetch_assoc()) {
 ?>
+     <li class="list-group-item"><?php echo $team['team_number']; ?> - <?php echo $team['location']; ?> - <?php echo $team['email']; ?></li>
 <?php 
   }
 ?>
-      </p>
+     </ul>
+     </p>
       <p class="card-text"><small class="text-body-secondary">Phone number: <?php echo $employee['phone_number']; ?></small></p>
     </div>
   </div>
