a1a165f918872ef0a4ab6b560d1fde4300d7f713
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 16:22:54 -0600

    Update view-employees-newform.php

--- view-employees-newform.php
+++ view-employees-newform.php
@@ -1,3 +1,31 @@
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newEmployeeModal">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
       <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0"/>
-    </svg>
+     </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newEmployeeModal" tabindex="-1" aria-labelledby="newEmployeeModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newEmployeeModalLabel">New employee</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <form method="post" action="">
+          <div class="mb-3">
+            <label for="eName" class="form-label">Employee name</label>
+            <input type="text" class="form-control" id="eName" name="eName">
+          </div>
+          <div class="mb-3">
+            <label for="ePhone" class="form-label">Employee phone</label>
+            <input type="text" class="form-control" id="ePhone" name="ePhone">
+          </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form> 
+      </div>
+    </div>
+  </div>
+</div>
