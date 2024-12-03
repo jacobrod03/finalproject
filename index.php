c729872a01cf98e18e999ae654fe4bd24c37a26d
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-02 22:36:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<style>
-    body {
-        background-image: url('https://www.oso-web.com/wp-content/uploads/2018/04/Image_1-4.jpeg');
-        background-size: cover; /* Ensures the image covers the entire background */
-        background-repeat: no-repeat; /* Prevents the image from repeating */
-        background-attachment: fixed; /* Keeps the image fixed when scrolling */
-    }
-</style>
 <h1>MIS 4013 Final Project</h1>
 <?php 
 include "view-footer.php";
