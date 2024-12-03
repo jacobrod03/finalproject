128b8d4cf65acfc0cb9ac14a3e6395eee84fa87c
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 14:39:45 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add": 
-    insertTeams($_POST['tNumber'], $_POST['tDesc']);
+    if (insertTeams($_POST['tNumber'], $_POST['tDesc'])) {
+      echo '<div class="alert alert-success" role="alert">Team added!</div>';
+    } else {
+      echo '<div class="alert alert-success" role="alert">Error.</div>';
+    }
     break;
   }
 }
