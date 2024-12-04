a211656c261669cb3eb9aaff58572da87c155a7e
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 13:22:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include "view-header.php";
             background-size: 100%;
             background-repeat: no-repeat;
             background-position: center;
-            height: 90vh;
+            height: 70vh;
             color: yellow; //adjust color for readability
             font-family: Arial, sans-serif;
         }
