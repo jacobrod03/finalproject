2c1a9401668582e454022205b31ed09e541754bb
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 20:57:34 -0600

    Update employees.php

--- employees.php
+++ employees.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
      }
       break;
+    case "Delete":
+     if (deleteEmployees($_POST['eid'])) {
+       echo '<div class="alert alert-success" role="alert">Employee deleted!</div>';
+     } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+     }
+      break;
   }
 }
 
