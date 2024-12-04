3f3ce37179d7fbb5ed775176399d494b8711fa8f
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 14:50:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "view-header.php";
             margin: 0;
             padding: 0;
             background-image: url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWtnZHJseW93djN4bjVtYnd5ajg5eGNyZWFnNmFsODhlZ2hxM2g5YSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/RiykPw9tgdOylwFgUe/giphy.gif');
-            background-size: contain;
+            background-size: 100%;
             background-repeat: no-repeat;
             background-position: center center;
             height: 100vh;
