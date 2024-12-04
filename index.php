9e502c9f294707e8361598418a15db4b9dca332e
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 14:58:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include "view-header.php";
         body {
             margin: 0;
             padding: 0;
-            background-image: url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWtnZHJseW93djN4bjVtYnd5ajg5eGNyZWFnNmFsODhlZ2hxM2g5YSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/RiykPw9tgdOylwFgUe/giphy.gif');
+            background-image: url('https://tenor.com/view/apply-today-apply-today-ice-group-galway-gif-16159688');
             background-size: 100%;
             background-repeat: no-repeat;
             background-position: center center;
