78be4e2fc1155395af1e304dbebdc8c513603e9c
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 14:38:27 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,14 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-      .header-lightblue {
-        background-color: lightblue;
-      }
-    </style>
   </head>
   <body>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary header-lightblue">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand fw-bold" href="#">FIND YOUR FIT TODAY!</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
