581e5015fd460c507b7c8285c1b01a34a51755a0
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 12:13:18 -0600

    Create contact-us.php

---
+++ contact-us.php
@@ -0,0 +1,16 @@
+<form>
+  <div class="mb-3">
+    <label for="exampleInputName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="exampleInputName" aria-describedby="nameHelp">
+    <div id="nameHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPhone" class="form-label">Phone number</label>
+    <input type="tel" class="form-control" id="exampleInputPhone">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
