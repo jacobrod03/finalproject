28d69c52457116eaee9d255e6fc9c60211b41c60
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 14:37:17 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -13,29 +13,31 @@
   </head>
   <body>
       <nav class="navbar navbar-expand-lg bg-body-tertiary header-lightblue">
-  <div class="container-fluid">
-    <a class="navbar-brand fw-bold" href="#">FIND YOUR FIT TODAY!</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active fw-bold" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold" href="employees.php">Employees</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold" href="teams.php">Teams</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold" href="employees-with-teams.php">Employees with teams</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link fw-bold" href="employees-chart.php">Employees chart</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div class="container-fluid">
+          <a class="navbar-brand fw-bold" href="#">FIND YOUR FIT TODAY!</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0 justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link active fw-bold" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="employees.php">Employees</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="teams.php">Teams</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="employees-with-teams.php">Employees with teams</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="employees-chart.php">Employees chart</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  </body>
+</html>
