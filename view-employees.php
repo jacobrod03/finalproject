e1b278074bf1d2a5e9952900ba6c6091d24b6371
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-03 20:55:43 -0600

    Update view-employees.php

--- view-employees.php
+++ view-employees.php
@@ -30,7 +30,15 @@ while ($employee = $employees->fetch_assoc()) {
       
     </td>
     <td>
-      
+      <form method="post" action="">
+        <input type="hidden" name="eid" value="<?php echo $employee['employee_id']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+          </svg>
+        </button>
+      </form>
     </td>
     <td><a href="teams-by-employee.php?id=<?php echo $employee['employee_id']; ?>">Teams</a></td>
   </tr>
