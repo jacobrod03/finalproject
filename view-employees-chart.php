7373c78987c5cd6973aff5ae977b2fdff12d14be
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-12-04 19:19:37 -0600

    Update view-employees-chart.php

--- view-employees-chart.php
+++ view-employees-chart.php
@@ -1,5 +1,5 @@
 <h1>Chart of employees</h1>
-<div>
+<div style="background-color: #d4edda; padding: 20px; border-radius: 10px;">
   <canvas id="myChart" width="300" height="300"></canvas>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -33,7 +33,7 @@ while ($employee = $employees->fetch_assoc()) {
       options: {
       responsive: true,
       maintainAspectRatio: true,
-      aspectRatio: 2.5, // Adjusted to shrink the chart size
+      aspectRatio: 2.5, 
     }
   });
 </script>
